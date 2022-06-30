45b2e4db1c5fc5f2819b6850ea1f4ce55cd0b23d
JessiJess <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-06-30 16:20:40 +0200

    added new styles to contact container

--- src/views/ContactRegister.js
+++ src/views/ContactRegister.js
@@ -1,5 +1,5 @@
 import React,{ useState} from 'react';
-import '../stylesheets/ContactRegister.css';
+import '../stylesheets/ContactRegister.css'
 
 
 function ContactRegister(){
@@ -7,10 +7,10 @@ function ContactRegister(){
     const [phone, setPhone] = useState("");
     const [fullName, setFullName] = useState("");
     const [photo, setPhoto] = useState("");
-    
+
     const handleSubmit = event => {
         event.preventDefault();
-        fetch("https://hackathon-final.herokuapp.com/contact/create", {
+        let res = fetch("https://hackathon-final.herokuapp.com/contact/create", {
             method: "POST",
             body: JSON.stringify({
         phone: phone,
@@ -18,22 +18,19 @@ function ContactRegister(){
         photo: photo,
         }),
         });
-
     }
 
     return(
-        <div>
-            <form>
-                <input type="text" id="phone" name="phone" placeholder="telefono" onChange={(e) => setPhone(e.target.value)}></input>
-                <input type="text" id="fullName" name="fullName" placeholder="Nombre completo" onChange={(e) => setFullName(e.target.value)}></input>
-                <input type="email" id="photo" name="photo" placeholder="photo" onChange={(e) => setPhoto(e.target.value)}></input>
-
-                <select name="asignacion" id="asignacion">
+        <div className="customcontainer">
+            <form onSubmit={handleSubmit}>
+                <input className="input" type="text" id="phone" name="phone" placeholder="telefono" onChange={(e) => setPhone(e.target.value)}></input>
+                <input  className="input" type="text" id="fullName" name="fullName" placeholder="Nombre completo" onChange={(e) => setFullName(e.target.value)}></input>
+                <select  className="select" name="asignacion" id="asignacion">
                     <option value="green">He llegado</option>
                     <option value="yellow">Necesito Ayuda</option>
                     <option value="red">Llamar 112</option>
                 </select>
-                <button className="addbutton" onClick={handleSubmit} type="submit">Añadir contacto</button>
+                <button className="addbutton" type="submit">Añadir contacto</button>
             </form>
         </div>
     );
