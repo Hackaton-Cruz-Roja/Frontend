0cafb02d4024f5c69d8b5080c957749c3d7ecc68
ginkgo <alexvid33@gmail.com>
2022-06-30 11:52:30 +0200

    geolocalization component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import Login from './components/Login'
+import Geolocalization from './components/Geolocalization'
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <Login />
+    <Geolocalization />
 
   </React.StrictMode>
 );
