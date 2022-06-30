446d2ca426eafaa558f3eaa9fd8f9f5c6c0ff6ab
JessiJess <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-06-30 12:12:49 +0200

    stabilized jess branch

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
-import App from './App';
-=======
-//import App from './App';
-import Maps from './components/Maps'
-//import reportWebVitals from './reportWebVitals';
->>>>>>> 0cafb02d4024f5c69d8b5080c957749c3d7ecc68
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <Maps />
-
   </React.StrictMode>
 );
 
-<<<<<<< HEAD
-=======
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-//reportWebVitals();
->>>>>>> 0cafb02d4024f5c69d8b5080c957749c3d7ecc68
