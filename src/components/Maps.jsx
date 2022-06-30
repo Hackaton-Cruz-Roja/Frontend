a5fb838b0f0555ee4022eb0625fef8623f5aaccc
ginkgo <alexvid33@gmail.com>
2022-06-30 12:07:08 +0200

    changed LoginComp and RegisterComp naming

---
+++ src/components/Maps.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+    GoogleMap,
+    withScriptsjs,
+    
+}
+
+const Maps = (props) => {
+  return (
+    <div>Maps</div>
+  )
+}
+
+export default Maps
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import Geolocalization from './components/Geolocalization'
+import Maps from './components/Maps'
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <Geolocalization />
+    <Maps />
 
   </React.StrictMode>
 );
