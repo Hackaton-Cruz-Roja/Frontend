a90a426c14c315da3c8fe2a9e9ee6b85610b6d2d
ginkgo <alexvid33@gmail.com>
2022-06-30 13:09:20 +0200

    implement map component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import Maps from './components/Maps'
+import Map from './components/Map';
 //import reportWebVitals from './reportWebVitals';
+//import RegisterComp from './components/RegisterComp'
+//import LoginComp from './components/LoginComp'
+
+
+//const mapURL = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${credentials.mapsKey}`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <Maps />
+    <Map />
 
   </React.StrictMode>
 );
