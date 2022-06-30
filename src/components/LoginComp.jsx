9befa069f58828d531ad6ae74a9529fd0a63b47c
ginkgo <alexvid33@gmail.com>
2022-06-30 16:17:30 +0200

    user can register

--- src/components/LoginComp.jsx
+++ src/components/LoginComp.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../stylesheets/LoginComp.css';
 import ProfileIcon from '../media/profileIcon.png';
 
 
 const LoginComp = () => {
+
+    function parseJwt (token) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        console.log(JSON.parse(jsonPayload));
+        return JSON.parse(jsonPayload);
+    };
+
+    const [identificator, setIdentificator] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    async function signIn() {
+        let item = {identificator, password}
+        console.warn(item);
+
+        let result = await fetch("https://hackathon-final.herokuapp.com/authenticate", {
+        method: 'POST',
+        body: JSON.stringify(item),
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        }
+
+    })
+
+    result = await result.json()
+    //console.warn("result", result)
+    //let token = parseJwt(result.data.token)
+    console.log(result.token);
+
+    }
+
     return (
         <section>
             <img className="profile-icon" src={ ProfileIcon } alt="profilepic"/>
-            <input className='loginInput' placeholder='Usuario' type="text" />
-            <input className='loginInput' placeholder='Contraseña' type="text" />
-            <button className="login-button" type='submit'>LogIn</button>
+            <input value={identificator} onChange={(e) => setIdentificator(e.target.value)} className='loginInput' placeholder='Usuario' type="text" />
+            <input value={password} onChange={(e) => setPassword(e.target.value)} className='loginInput' placeholder='Contraseña' type="password" />
+            <button onClick={signIn} className="login-button" type='submit'>LogIn</button>
+            
         </section>
     )
 }
--- src/components/RegisterComp.jsx
+++ src/components/RegisterComp.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import React,  {useState} from 'react'
 import '../stylesheets/RegisterComp.css'
 import ProfileIcon from '../media/profileIcon.png';
 
 const RegisterComp = () => {
+  
+  const [identificator, setIdentificator] = useState("")
+  const [name, setName] = useState("")
+  const [surname, setSurname] = useState("")
+  const [password, setPassword] = useState("")
+  const [mail, setMail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [observations, setObservations] = useState("")
+
+  async function signUp() {
+    let item = {identificator, name, surname, password, mail, phone, observations}
+    console.warn(item);
+
+    let result = await fetch("https://hackathon-final.herokuapp.com/register", {
+    method: 'POST',
+    body: JSON.stringify(item),
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+
+  })
+
+  result = await result.json()
+  console.warn("result", result)
+  }
+
   return (
     <section>
         <img className="profile-icon" src={ ProfileIcon } alt="profilepic"/>
 
-        <input className='registerInput' placeholder='DNI/NIE' type="text" />
-        <input className='registerInput' placeholder='Name' type="text" />
-        <input className='registerInput' placeholder='Surname' type="text" />
-        <input className='registerInput' placeholder='Password' type="text" />
-        <input className='registerInput' placeholder='Email' type="text" />
-        <input className='registerInput' placeholder='Phone number' type="text" />
-        <input className='registerInput' placeholder='Observations' type="text" />
-        <button type='submit'>Register</button>
+        <input value={identificator} onChange={(e) => setIdentificator(e.target.value)} className='registerInput' placeholder='DNI/NIE' type="text" />
+        <input value={name} onChange={(e) => setName(e.target.value)} className='registerInput' placeholder='Name' type="text" />
+        <input value={surname} onChange={(e) => setSurname(e.target.value)} className='registerInput' placeholder='Surname' type="text" />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} className='registerInput' placeholder='Password' type="password" />
+        <input value={mail} onChange={(e) => setMail(e.target.value)} className='registerInput' placeholder='Email' type="text" />
+        <input value={phone} onChange={(e) => setPhone(e.target.value)} className='registerInput' placeholder='Phone number' type="text" />
+        <input value={observations} onChange={(e) => setObservations(e.target.value)} className='registerInput' placeholder='Observations' type="text" />
+        <button onClick={signUp} type='submit'>Register</button>
     </section>
   )
 }
