f6b7d1ae14c2caf7092bd63414e3d9ad9646c222
JessiJess <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-06-30 12:38:00 +0200

    wrote use navigate logic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,20 @@
 import '../stylesheets/NavBar.css';
 import {CgMenuBoxed} from 'react-icons/cg';
 import 'bootstrap/dist/css/bootstrap.css';
+// import { useNavigate } from 'react-router-dom';
 import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
 import React,{useState} from 'react';
 
 
 function NavBar(){
 
+            // const navigate = useNavigate();
             const [dropdown, setDropdown] = useState(false);
     
             const openCloseDropdown = () => {
                 setDropdown(!dropdown);
             }
+
     return(
 
         <div className='navbar-container'>
@@ -21,8 +24,8 @@ function NavBar(){
                 </DropdownToggle >
 
                 <DropdownMenu>
-                <DropdownItem> Home </DropdownItem>
-                    <DropdownItem> Añadir botones </DropdownItem>
+                    <DropdownItem> Home </DropdownItem>
+                    <DropdownItem > Añadir botones </DropdownItem>
                     <DropdownItem> Añadir contactos </DropdownItem>
                 </DropdownMenu>
 
