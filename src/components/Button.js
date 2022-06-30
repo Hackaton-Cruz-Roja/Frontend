b3093ce23fa7ad25e64b012317c2ba2d4f4edf8d
JessiJess <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-06-30 13:51:01 +0200

    restyled login component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,16 @@
 // import React, {useEffect} from 'react'
 import '../stylesheets/Button.css'
 
+
+
+function Button({text, onClick}){
+    
 // useEffect(() => {
 //     const response = await fetch('https://hackathon-final.herokuapp.com/configuration/create%27)
 //         .then(response => response.json())
 //         .then(data=> text = data);
 // }, []);
 
-function Button({text, onClick}){
     return(
         <div
         className="button"
